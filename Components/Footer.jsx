d8c4926dd8bb3dd737f94f01c448641c84e83e2b
Alexandre Cabral <alex.miguel89@gmail.com>
2024-05-14 09:13:09 +0100

    new footer

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -3,7 +3,7 @@ import { Box } from "@chakra-ui/react";
 function Footer() {
   return (
     <Box as="footer" bg="white.800" color="black" p={4} mt="auto">
-      Github Repository
+      <a href="https://github.com/kafdhali/champcraftlol" target="_">Github Repository</a>  
     </Box>
   );
 }
