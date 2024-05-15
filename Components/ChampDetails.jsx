6106be6a8ad2d03b2a554536bfc63e9f4b2fc014
kafdhali <khaledafdh075@gmail.com>
2024-05-15 09:33:21 +0200

    fix champcard

--- Components/ChampDetails.jsx
+++ Components/ChampDetails.jsx
@@ -1,11 +1,12 @@
-import axios from "axios";
-import { Box, Heading, Text, Image } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { Box, Heading, Text, Image, Skeleton } from "@chakra-ui/react";
 
 function ChampDetails() {
   const [champion, setChampion] = useState(null);
   const { championId } = useParams();
+
   useEffect(() => {
     axios
       .get(`https://champcraft.adaptable.app/champions/${championId}`)
@@ -18,25 +19,41 @@ function ChampDetails() {
   }, [championId]);
 
   if (!champion) {
-    return <div className="loader"></div>;
+    return <Skeleton height="300px" />;
   }
 
   return (
-    <Box maxW="xl" borderWidth="1px" borderRadius="lg" overflow="hidden" p={4}>
-      <Image src={champion.thumbnail} alt={champion.title} />
+    <Box
+      maxW="xl"
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      p={4}
+      boxShadow="md" // Added box shadow
+      textAlign="center" // Centered content
+    >
+      <Text fontSize="lg" fontWeight="bold" mb="2">
+        {champion.name}
+      </Text>
+      <Image src={champion.thumbnail} alt={champion.title} mx="auto" />{" "}
+      {/* Centered image */}
       <Box p="6">
-        <Box d="flex" alignItems="baseline">
-          <Heading as="h2" size="lg" mr="2">
-            {champion.title}
-          </Heading>
-          <Text color="gray.500" fontSize="sm">
-            Lane: {champion.lane}
-          </Text>
-        </Box>
-        <Text mt="2" fontSize="sm" fontWeight="semibold" lineHeight="short">
-          Description: {champion.description}
+        <Heading as="h2" size="xl" mb="2" fontWeight="bold">
+          {" "}
+          {/* Made title bigger */}
+          {champion.title}
+        </Heading>
+        <Text color="gray.500" fontSize="lg" fontWeight="bold" mb="4">
+          {" "}
+          {/* Made Lane bold and bigger */}
+          Lane: {champion.lane}
         </Text>
-        <Text mt="2" fontSize="sm" fontWeight="semibold" lineHeight="short">
+        <Text fontSize="lg" lineHeight="short" mb="4">
+          {" "}
+          {/* Made description bigger */}
+          {champion.description}
+        </Text>
+        <Text fontSize="lg" fontWeight="semibold">
           Price: {champion.price}
         </Text>
       </Box>
